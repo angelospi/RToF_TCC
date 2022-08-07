3e1d8f50f8cb7e6a7fd752a4fa6be0ffa39ec390
angelospi <angelo.spinardi@hotmail.com>
2022-08-07 18:43:41 -0300

    Script para rodar os cenários automaticamente

---
+++ rodar_cenarios_especificos.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+./rtof1 -u Cmdenv -c Wireless_Circle_3h_1ms
+./rtof1 -u Cmdenv -c Wireless_Circle_3h_15ms
+./rtof1 -u Cmdenv -c Wireless_Circle_3h_30ms
+./rtof1 -u Cmdenv -c Wireless_Circle_6h_1ms
+./rtof1 -u Cmdenv -c Wireless_Circle_6h_15ms
+./rtof1 -u Cmdenv -c Wireless_Circle_6h_30ms
+./rtof1 -u Cmdenv -c Wireless_Circle_9h_1ms
+./rtof1 -u Cmdenv -c Wireless_Circle_9h_15ms
+./rtof1 -u Cmdenv -c Wireless_Circle_9h_30ms
